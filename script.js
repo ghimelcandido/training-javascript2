40175146da18b16bf5292f8a55019c2cb4d6a9ea
Ghimel Cândido <105327320+ghimelcandido@users.noreply.github.com>
2023-12-29 01:01:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const listaDeAlunos = [
+    {
+        nome: "Ghimel",
+        nota1: 6,
+        nota2: 7
+    },
+    {
+        nome: "Amalia",
+        nota1: 10,
+        nota2: 10 
+    },
+    {
+        nome: "Bruno",
+        nota1: 9,
+        nota2: 9 
+    }
+]
+
+function resultadoMedia(nota1, nota2){
+    media = (nota1 + nota2) / 2
+    return media
+}
+
+for (let alunos of listaDeAlunos) {
+    let resultadoFinal = resultadoMedia(alunos.nota1, alunos.nota2)
+    let aprovadoOuReprovado = resultadoFinal > 7 ? `Aprovado` : `Reprovado`
+
+    alert(`A media do aluno(a) ${alunos.nome} é: ${resultadoFinal}. Você foi ${aprovadoOuReprovado} `)
+}
